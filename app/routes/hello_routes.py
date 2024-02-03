5ba88d5b642defc5202e8e399c8a71c5aa6cc582
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2024-02-03 13:43:07 +0400

    Update hello_routes.py

--- app/routes/hello_routes.py
+++ app/routes/hello_routes.py
@@ -7,7 +7,7 @@ class HelloRoute:
     @staticmethod
     async def get(request):
         # conn = request.ctx.conn
-        user = User("user_login", "pass_hash", "first_name", "last_name")
-        async with request.ctx.conn as conn:
-            conn.commit()
+        # user = User("user_login", "pass_hash", "first_name", "last_name")
+        # async with request.ctx.conn as conn:
+        #     conn.commit()
         return json({"res": "Hello, world."})
